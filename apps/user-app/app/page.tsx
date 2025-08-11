3c1b3f7bc87b3a591b53ca267413aab74097efa5
Gani0230 <ganeshjadhav7478@gmail.com>
2025-08-11 23:47:26 +0530

    initialize prisma in packages

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,12 @@
 import { Appbar } from "@repo/ui/appbar";
+import { PrismaClient } from "@repo/db/db"
 
+const client = new PrismaClient()
 export default function Home() {
+  console.log("hi there")
+  console.log(client.user)
+  console.log("hi there")
+
   return (
     <div>
       <Appbar />
---
+++ apps/user-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",         
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}"  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
